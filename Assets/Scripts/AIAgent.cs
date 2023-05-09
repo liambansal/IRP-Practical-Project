9dbf31d07b185d80fd0ae884e8c53efcd687a6f6
liambansal <55756009+liambansal@users.noreply.github.com>
2023-05-09 17:06:30 +0100

    Outlined AI Actions
    Outlined some actions for the AI agent.

--- Assets/Scripts/AIAgent.cs
+++ Assets/Scripts/AIAgent.cs
@@ -1,18 +1,14 @@
+// Written by Liam Bansal
+// Date Created: 9/5/2023
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIAgent : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class AIAgent : MonoBehaviour {
+	private void Follow() { }
+	private void MoveTo() { }
+	private void PickUp() { }
+	private void Drop() { }
+	private void Stay() { }
 }
